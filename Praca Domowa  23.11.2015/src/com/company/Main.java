acaf2f0186fa793e412891f1e389b027e9695f1a
ArekGebicki1995 <gebicki15@wp.pl>
2015-12-05 13:39:37 +0100

    Update Main.java

--- Praca Domowa  23.11.2015/src/com/company/Main.java
+++ Praca Domowa  23.11.2015/src/com/company/Main.java
@@ -1,3 +1,4 @@
+
 package com.company;
 
 import java.util.Scanner;
@@ -12,39 +13,29 @@ public class Main {
         Scanner cenawpisz = new Scanner(System.in);
         int cena = cenawpisz.nextInt();
 
-        System.out.println("20");
+        System.out.println("ilosc rat");
 
 //        Scanner ilerat = new Scanner(System.in);
         int rata = cenawpisz.nextInt();
         cenawpisz.close();
 
-        double wynik1 = cena*2.5/rata;
-        double wynik11 = cena*2.5;
-        double wynik2 = cena*5/rata;
-        double wynik21 = cena*5;
-        double wynik3 = cena*10/rata;
-        double wynik31 = cena*10;
+        double wynik1 = cena*0.025/rata;
+        double wynik11 = cena*rata;
+        double wynik2 = cena/rata+cena/rata*0.05;
+        double wynik21 = wynik2*rata;
+        double wynik3 = cena/rata+cena/rata*0.1;
+        double wynik31 = wynik3*0.1;
 
         if(rata<=12){
             System.out.println("Twoja rata wyniesie " + wynik1);
-            System.out.println("Przy " + rata + " ratach kwota ca³kowita wyniesie " + wynik11);
+            System.out.println("Przy " + rata + " ratach kwota caÅ‚kowita wyniesie " + wynik11);
         }
-        if(rata<=24){
+         else if(rata<=24){
             System.out.println("Twoja rata wyniesie " + wynik2);
-            System.out.println("Przy " + rata + " ratach kwota ca³kowita wyniesie " + wynik21);
+            System.out.println("Przy " + rata + " ratach kwota caÅ‚kowita wyniesie " + wynik21);
         }
-        if(rata<=48){
+        else if(rata<=48){
             System.out.println("Twoja rata wyniesie " + wynik3);
-            System.out.println("Przy " + rata + " ratach kwota ca³kowita wyniesie " + wynik31);
+            System.out.println("Przy " + rata + " ratach kwota caÅ‚kowita wyniesie " + wynik31);
+
         }
-
-
-
-
-
-
-
-
-
-    }
-}
